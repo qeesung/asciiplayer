f4ed07db18a12b57a204711bf318d7986f60539f
qeesung <1245712564@qq.com>
2018-11-09 15:42:08 +0800

    Add image file type check

--- pkg/util/filetype.go
+++ pkg/util/filetype.go
@@ -8,3 +8,17 @@ func IsGif(filename string) bool {
 	extension := filepath.Ext(filename)
 	return extension == ".gif"
 }
+
+func IsSupportedImage(filename string) bool {
+	extension := filepath.Ext(filename)
+	switch extension {
+	case ".jpeg":
+		fallthrough
+	case ".jpg":
+		fallthrough
+	case ".png":
+		return true
+	default:
+		return false
+	}
+}
