ddb2d0ecc5f22183a2f53ceaa1137a83e8670113
秦世成 <qeesung@live.com>
2018-11-10 12:45:10 +0800

    Add the flusher.go documents

--- pkg/remote/flusher.go
+++ pkg/remote/flusher.go
@@ -10,11 +10,15 @@ import (
 	"time"
 )
 
+// FlushHandler define the basic oprations that flush image to remote server
 type FlushHandler interface {
 	Init() error
 	HandlerFunc() func(w http.ResponseWriter, r *http.Request)
 }
 
+// supportedFlushHandlerMatchers register the supported flush handler
+// and if the Match function is return true, just call the constructor  
+// function to build the flusher handler.
 var supportedFlushHandlerMatchers = []struct {
 	Match       func(string) bool
 	Constructor func(string, *convert.Options) FlushHandler
@@ -39,19 +43,23 @@ func NewFlushHandler(filename string, options *convert.Options) (handler FlushHa
 	return nil, false
 }
 
+// BaseFlushHandler is a basic flush handler that define some basic opration
 type BaseFlushHandler struct {
 }
 
+// Init doing nothing in base flush handler
 func (handler *BaseFlushHandler) Init() error {
 	return nil
 }
 
+// HandlerFunc return a empty hadnler function
 func (handler *BaseFlushHandler) HandlerFunc() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 	}
 }
 
+// Flush flush the string to remote client immediately
 func (handler *BaseFlushHandler) Flush(w http.ResponseWriter, s string) error {
 	fmt.Fprintf(w, s)
 	time.Sleep(time.Duration(100) * time.Millisecond)
